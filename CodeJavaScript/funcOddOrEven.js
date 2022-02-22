1028d77f84d9e0ae7b75b2f21be5c51617cac165
Jhon <jhonjlinaresb@gmail.com>
2022-02-22 20:39:16 +0100

    Eight layer error

--- CodeJavaScript/funcOddOrEven.js
+++ CodeJavaScript/funcOddOrEven.js
@@ -1,5 +1,6 @@
 /*
 English: Create a **function** to tell me if a number entered by a user is odd or even.
+Odd is Impar ; Even es Par
 Spanish: Crear una **función** que me diga si un numero ingresado por usuario es par o impar.
 */
 
@@ -9,17 +10,17 @@ function OddEven(num) {
     let result = (num % 2);
     if( result === 0)
     {
-        let oddResult = ('The ' + num + ' Is Odd'); // alert('The ' + num + ' Is Odd')); -> Use to display alert on screen ; Usar para mostrar alerta en pantalla
-        console.log(oddResult);
+        let evenResult = ('The ' + num + ' Is Even'); // alert('The ' + num + ' Is Even')); -> Use to display alert on screen ; Usar para mostrar alerta en pantalla
+        console.log(evenResult);
     }
     else
     {
-        let evenResult = ('The ' + num + ' Is Even'); // alert('The ' + num + ' Is Even')); -> Use to display alert on screen ; Usar para mostrar alerta en pantalla
-        console.log(evenResult);
+        let oddResult = ('The ' + num + ' Is Odd'); // alert('The ' + num + ' Is Odd')); -> Use to display alert on screen ; Usar para mostrar alerta en pantalla
+        console.log(oddResult);
     }
 }
 console.info(OddEven);
 OddEven(num);
 
-/*Documentation about this code:
+/* Documentation about this code:
 https://docs.google.com/document/d/11mltUohmbsOksPwzgAkIWKShYyem58L2TMtWKpYcfx0/edit?usp=sharing */
